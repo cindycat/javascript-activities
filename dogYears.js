1b6b0887a6667af3c3d8270f2147a911710cf44b
cindycat <reilyncatabui@gmail.com>
2021-11-17 17:53:48 +0100

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,29 @@
+//My age as a human
+let myAge = 28;
+
+//This variable is your early years
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//This variable is your later years
+let laterYears = myAge - 2;
+
+//dog years
+laterYears = laterYears * 4;
+
+console.log('earlyYears: ' + earlyYears, '\n' + 'laterYears: ' + laterYears);
+
+//This is your age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+//Create a new string variable which will be all small letter due to the .toLowerCase() method
+let myName = "Cindy".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
+
+
