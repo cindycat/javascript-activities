60a5ddaf92edb81a399088b96ef157323184ffe5
cindycat <reilyncatabui@gmail.com>
2021-11-18 23:18:08 +0100

    Create sleepDebtCalculator.js

---
+++ sleepDebtCalculator.js
@@ -0,0 +1,59 @@
+const getSleepHours = day => {
+  switch(day){
+    case 'Monday':
+    return 8;
+    break;
+    case 'Tuesday':
+    return 7;
+    break;
+    case 'Wednesday':
+    return 6;
+    break;
+    case 'Thursday':
+    return 7;
+    break;
+    case 'Friday':
+    return 9;
+    break;
+    case 'Saturday':
+    return 5;
+    break;
+    case 'Sunday':
+    return 7;
+    break;
+  }
+}
+//console.log(getSleepHours('Tuesday'));
+
+const getActualSleepHours = () => 
+getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday');
+
+
+const getIdealSleepHours = () => {
+    let idealSleepHours = 7;
+    return idealSleepHours * 7;
+}
+
+//console.log(getActualSleepHours());
+//console.log(getIdealSleepHours());
+
+
+const calculateSleepDebt = () => {
+  let actualSleepHours = getActualSleepHours();
+  let idealSleepHours = getIdealSleepHours();
+ // let overSleepHours = getOverSleepHours();
+
+ if( actualSleepHours === idealSleepHours ){
+    return "Congratulations! You got your ideal amount of sleep!";
+ } 
+  else if( actualSleepHours > idealSleepHours ){
+    return 'You sleep ' + (actualSleepHours - idealSleepHours) +  ' hours more than your ideal sleep hours.';
+  }
+  else if( actualSleepHours < idealSleepHours)
+    return ' You need ' + (idealSleepHours - actualSleepHours) + " more hour/s of sleep.";
+}
+
+console.log(calculateSleepDebt());
+ 
+
+
