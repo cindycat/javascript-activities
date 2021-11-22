1542ba1c6f8a1bf4b14d1cb0c652ad4cb1fde18a
cindycat <reilyncatabui@gmail.com>
2021-11-22 21:10:47 +0100

    Create secretMessage.js

---
+++ secretMessage.js
@@ -0,0 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+
+//console.log(secretMessage);
+//console.log(secretMessage.length);
+
+//secretMessage.push('Belat, kala mun deya, kala mu byaybi tukoy mu atanan anya ', 'deya ka beman t akala mu id?');
+
+//console.log(secretMessage);
+//console.log(secretMessage.shift());
+secretMessage.shift();
+
+
+
+
+
+
