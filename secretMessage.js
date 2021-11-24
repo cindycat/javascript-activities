97b688910a49521f776659a10e94e97ee58560ea
cindycat <reilyncatabui@gmail.com>
2021-11-24 15:46:48 +0100

    Update secretMessage.js

--- secretMessage.js
+++ secretMessage.js
@@ -1,15 +1,26 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
-secretMessage.pop();
+//secretMessage.pop();
 
 //console.log(secretMessage);
 //console.log(secretMessage.length);
 
 //secretMessage.push('Belat, kala mun deya, kala mu byaybi tukoy mu atanan anya ', 'deya ka beman t akala mu id?');
 
-//console.log(secretMessage);
-//console.log(secretMessage.shift());
-secretMessage.shift();
+//Use .splice method to delete/replace an item in an array or secretMessage[7] = 'right';
+//secretMessage.splice(7,1,"right");
+
+//Use .shift method to remove the first string of an array
+//secretMessage.shift();
+
+//Use .unshift to add string at the beginning of an array
+//secretMessage.unshift('Programming');
+
+//Use .splice method to remove a lot of strings and replace it with one string
+//secretMessage.splice(6,5, 'know');
+
+console.log(secretMessage.join());
+
 
 
 
